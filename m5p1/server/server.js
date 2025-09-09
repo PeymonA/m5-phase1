1e28fa3d0e5e9393d061d3b51883b679d911b981
PeymonA <peymon.armand4@gmail.com>
2025-09-10 10:55:12 +1200

    dockerfile

--- m5p1/server/server.js
+++ m5p1/server/server.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const itemRouter = require("./routes/itemRoutes.js");
+const fs = require('fs');
 
 const app = express();
 
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/m5-p1');
+fs.access('/.dockerenv', fs.constants.F_OK, (err) => {
+  if (!err) {
+      console.log('Running inside a Docker container (/.dockerenv exists)');
+      mongoose.connect('mongodb://host.docker.internal:27017/m5-p1');
+  } 
+  else {
+      console.log('Not running inside a Docker container (/.dockerenv not found)');
+      mongoose.connect('mongodb://localhost:27017/m5-p1');
+  }
+});
 
 app.use(itemRouter);
 
