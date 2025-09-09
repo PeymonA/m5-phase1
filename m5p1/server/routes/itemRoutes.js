73efc12ca60d33cac0168ea572d6362433b43be6
PeymonA <peymon.armand4@gmail.com>
2025-09-09 14:45:24 +1200

    Add patch method

--- m5p1/server/routes/itemRoutes.js
+++ m5p1/server/routes/itemRoutes.js
@@ -23,4 +23,20 @@ app.post("/item", async (request, response) => {
   }
 });
 
+app.patch("/item/:id", async (request, response) => {
+  try {
+    const updatedItem = await itemModel.findByIdAndUpdate(
+      request.params.id,
+      request.body,
+      { new: true } // return the updated document
+    );
+    if (!updatedItem) {
+      return response.status(404).send({ error: "Item not found" });
+    }
+    response.send(updatedItem);
+  } catch (error) {
+    response.status(500).send(error.message);
+  }
+});
+
 module.exports = app;
