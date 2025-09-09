ae4dfaec90d299b1e4868359f9e4de75fe4dde8c
PeymonA <peymon.armand4@gmail.com>
2025-09-09 20:45:43 +1200

    more test cases

--- m5p1/server/tests/getItems.test.js
+++ m5p1/server/tests/getItems.test.js
@@ -50,5 +50,70 @@ describe('GET /items/<keyword>', () => {
         await request(app).delete(`/items/iphone`);
     });
 
-    it.todo;
+    it('Does not get wrong item', async () => {
+        const waiting = await request(app).delete("/items");
+
+        const postBody1 = {
+                "title" : "iPhone",
+                "description" : "",
+                "start_price" : 1000,
+                "reserve_price" : 2000
+        };
+        await request(app).post('/item').send(postBody1);
+
+        const getRes = (await request(app).get(`/items/Samsung`));
+        expect(getRes.body.length).toBe(0);
+
+        await request(app).delete(`/items/iphone`);
+    });
+    it('Case insensitive', async () => {
+        const waiting = await request(app).delete("/items");
+
+        const postBody1 = {
+                "title" : "iPhone",
+                "description" : "",
+                "start_price" : 1000,
+                "reserve_price" : 2000
+        };
+        await request(app).post('/item').send(postBody1);
+
+        const getRes = (await request(app).get(`/items/IPHONE`));
+        expect(getRes.body.length).toBe(1);
+
+        await request(app).delete(`/items/iphone`);
+    });
+
+    it('Empty string', async () => {
+        const waiting = await request(app).delete("/items");
+
+        const postBody1 = {
+                "title" : "iPhone",
+                "description" : "",
+                "start_price" : 1000,
+                "reserve_price" : 2000
+        };
+        await request(app).post('/item').send(postBody1);
+
+        const getRes = (await request(app).get(`/items/""`));
+        expect(getRes.body.length).toBe(0);
+
+        await request(app).delete(`/items/iphone`);
+    });
+
+    it('Special characters', async () => {
+        const waiting = await request(app).delete("/items");
+
+        const postBody1 = {
+                "title" : "iPhone",
+                "description" : "",
+                "start_price" : 1000,
+                "reserve_price" : 2000
+        };
+        await request(app).post('/item').send(postBody1);
+
+        const getRes = (await request(app).get(`/items/!!!`));
+        expect(getRes.body.length).toBe(0);
+
+        await request(app).delete(`/items/iphone`);
+    });
 });
